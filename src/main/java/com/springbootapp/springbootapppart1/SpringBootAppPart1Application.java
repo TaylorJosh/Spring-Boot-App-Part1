aab043063a29ed639eb8218c8302fc1ed629fbb5
Josh Taylor <taylor.joshua05@gmail.com>
2022-05-31 14:49:11 -0400

    first commit

---
+++ src/main/java/com/springbootapp/springbootapppart1/SpringBootAppPart1Application.java
@@ -0,0 +1,13 @@
+package com.springbootapp.springbootapppart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppPart1Application.class, args);
+    }
+
+}
